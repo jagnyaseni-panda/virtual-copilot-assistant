7316fe8e6fff0be1ae57f5aadac49f8cd411fe88
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-06-16 23:49:29 +0530

    Virtual AI Assistant

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
+  connectDb()
   console.log(`Server is running on port ${PORT}`);
 });
