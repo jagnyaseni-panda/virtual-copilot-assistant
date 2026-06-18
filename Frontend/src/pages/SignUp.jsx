8afc1c5147989ed41467308fbcd32ab099fc84de
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-06-18 22:51:29 +0530

    Create SignUp & SignIn Page

---
+++ Frontend/src/pages/SignUp.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import bg from "../assets/authBg.png"
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+
+function SignUp() {
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleSignUp = async ()=>{
+        
+    }
+  return (
+    <div className="w-full h-[100vh] bg-cover flex items-center justify-center" style={{ backgroundImage: `url(${bg})` }}>
+        <form className="w-[90%] h-[600px] max-w-[500px] bg-[#00000062] backdrop-blur shadow-lg shadow-black flex flex-col items-center justify-center gap-[20px] px-[20px]">
+            <h1 className="text-white text-[30px] font-semibold mb-[30px]">Register to <span className="text-blue-400">Virtual Assistant</span></h1>
+            <input type="text" placeholder="Enter your name" className="w-full h-[60px] border-2 border-white bg-transparent  text-white placeholder-gray-300 outline-none px-[20px] py-[10px] rounded-full text-[18px]" required onChange={(e)=>setName(e.target.value)} value={name} />
+            <input type="email" placeholder="Email" className="w-full h-[60px] border-2 border-white bg-transparent  text-white placeholder-gray-300 outline-none px-[20px] py-[10px] rounded-full text-[18px]" required onChange={(e)=>setEmail(e.target.value)} value={email}  />
+            <div className="w-full h-[60px] border-2 border-white bg-transparent relative text-white rounded-full text-[18px] ">
+                <input type={showPassword ? "text" : "password"} placeholder="Password" className="w-full h-full rounded-full bg-transparent outline-none px-[20px] py-[10px] placeholder-gray-300" required onChange={(e)=>setPassword(e.target.value)} value={password}  />
+                {!showPassword && 
+                <IoEye className="absolute top-[18px] right-[20px] text-[white] w-[25px] h-[25px] text-[white] cursor-pointer" onClick={() => setShowPassword(true)} />}
+                {showPassword && (
+                <IoEyeOff className="absolute top-[18px] right-[20px] text-[white] w-[25px] h-[25px] text-[white] cursor-pointer" onClick={() => setShowPassword(false)} />
+                )}
+            </div>
+            <button className="min-w-[150px] h-[60px] mt-[30px] text-black font-semibold bg-white rounded-full text-[19px]">Sign Up</button>
+            <p className="text-white text-[18px] cursor-pointer" onClick={() => navigate("/signin")}>
+                Already have an account? <span className="text-blue-400 ">Sign In</span>
+            </p>
+        </form>
+    </div>
+  )
+}
+
+export default SignUp
