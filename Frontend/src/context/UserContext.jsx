d84e7509d09b1b1d98d0f2c478648671c9aba6f3
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-06-20 21:42:38 +0530

    Create SignUp & SignIn Page

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,11 +1,32 @@
 import React, { createContext } from 'react'
+import { useEffect } from 'react'
 
 export const userDataContext = createContext()
 function UserContext(children) {
+  const serverUrl = "http://localhost:9000"
+  const [userData,setUserData] = useState(null)
+
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
+      setUserData(result.data)
+      console.log(result.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+    useEffect(() => {
+      handleCurrentUser()
+  },[])
+
+  const value = {
+    serverUrl,userData,setUserData
+  }
   return (
     <div>
-        <userDataContext.Provider>
-            {children}
+        <userDataContext.Provider value={value}>
+          {children}
         </userDataContext.Provider>
     </div>
   )
