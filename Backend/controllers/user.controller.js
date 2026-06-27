c833c41046327ee63add464157139d9f39ffbdd1
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-06-27 11:26:35 +0530

    Create Update Assistant APIs

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 
 export const getCurrentUser = async (req,res) => {
@@ -10,5 +11,23 @@ export const getCurrentUser = async (req,res) => {
         res.status(200).json(user)
     }catch(error){
         return res.status(400).json({message: "Get current user error"})
+    }
+}
+
+export const updateAssistant = async(req,res)=>{
+    try {
+        const {assistantName,imageUrl} = req.body
+        let assistantImage;
+        if(req.file){
+            assistantImage = await uploadOnCloudinary(req.file.path)
+        }else{
+            assistantImage = imageUrl
+        }
+        const user = await User.findByIdAndUpdate(req.userId,{
+            assistantName,assistantImage
+        },{new:true}).select("-password")
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(400).json({message: "Get Update Assistant error"})
     }
 }
