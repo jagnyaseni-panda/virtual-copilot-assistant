744040fc4528b18ece929a946a8560134342cad5
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-07-14 20:24:29 +0530

    Virtual AI Assistant

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.routes.js";
+import geminiResponse from "./gemini.js";
 
 dotenv.config();
 
@@ -20,6 +21,12 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
+
+// app.get("/",async(req,res) =>{
+//   let prompt = req.query.prompt
+//   let data = await geminiResponse(prompt)
+//   res.json(data)
+// })
 
 app.listen(PORT, () => {
   connectDb()
